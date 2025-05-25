5eab215679607ebf1da202be03951856f40d5a3b
nibelought <nibeloughtgamer.x@gmail.com>
2025-05-26 02:45:51 +0300

    Add intefaces

---
+++ FileProcessorApp/Interfaces.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FileProcessorApp
+{
+    // ----- Abstract Products: Loaders -----
+    public interface ILoader
+    {
+        string Load(Stream stream); // Returns content ready for display
+    }
+
+    // ----- Abstract Products: Savers -----
+    public interface ISaver
+    {
+        void Save(Stream stream, string content); // Content is the edited text from screen
+    }
+
+    // ----- Abstract Factory -----
+    public interface IFileFormatFactory
+    {
+        ILoader CreateLoader();
+        ISaver CreateSaver();
+    }
+
+    // ----- File Type Enum -----
+    public enum FileType
+    {
+        HTML,
+        TXT,
+        BIN,
+        Unknown
+    }
+}

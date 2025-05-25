10cd787c65db2a078468201b4e7595de2117a4d7
nibelought <nibeloughtgamer.x@gmail.com>
2025-05-26 02:47:58 +0300

    Factories.cs

---
+++ FileProcessorApp/Factories.cs
@@ -0,0 +1,20 @@
+﻿namespace FileProcessorApp
+{
+    public class HtmlFileFactory : IFileFormatFactory
+    {
+        public ILoader CreateLoader() => new HtmlLoader();
+        public ISaver CreateSaver() => new HtmlSaver();
+    }
+
+    public class TxtFileFactory : IFileFormatFactory
+    {
+        public ILoader CreateLoader() => new TxtLoader();
+        public ISaver CreateSaver() => new TxtSaver();
+    }
+
+    public class BinFileFactory : IFileFormatFactory
+    {
+        public ILoader CreateLoader() => new BinLoader();
+        public ISaver CreateSaver() => new BinSaver();
+    }
+}
